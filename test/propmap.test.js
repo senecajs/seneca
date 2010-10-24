6d2976ba93bfb83e7197b4028c4ea1cedddae30f
Richard Rodger <richard@ricebridge.com>
2010-10-24 22:58:29 +0100

    add

--- test/propmap.test.js
+++ test/propmap.test.js
@@ -62,7 +62,38 @@ module.exports = {
 
     pm.root = {root:true,prop:'root',star:{prop:'p4',subs:{v4:{prop:'p5',subs:{v5:{ref:'r6'}}}}}}; //sys.puts(pm.toString());
     assert.equal('PropMap:root:\n * -> p4:\n v4 -> p5:\n v5 -> :\n r6\n\n',pm.toString().replace(/ +/g,' '));
+  },
+
+
+  add: function(assert) {
+    var pm = new PropMap();
+    
+    pm.add( {p1:'v1'}, 'r1' ); //sys.puts(pm.toString());
+    assert.equal('r1',pm.find({p1:'v1'}));
+    assert.equal(null,pm.find({p2:'v1'}));
+    assert.equal('PropMap:root:\n * -> p1:\n v1 -> :\n r1\n',pm.toString().replace(/ +/g,' '));
+
+    pm.add( {p1:'v1'}, 'r1x' ); //sys.puts(pm.toString());
+    assert.equal('r1x',pm.find({p1:'v1'}));
+    assert.equal(null,pm.find({p2:'v1'}));
+    assert.equal('PropMap:root:\n * -> p1:\n v1 -> :\n r1x\n',pm.toString().replace(/ +/g,' '));
+
+    pm.add( {p1:'v2'}, 'r2' ); //sys.puts(pm.toString());
+    assert.equal('r2',pm.find({p1:'v2'}));
+    assert.equal(null,pm.find({p2:'v2'}));
+    assert.equal('PropMap:root:\n * -> p1:\n v1 -> :\n r1x\n v2 -> :\n r2\n',pm.toString().replace(/ +/g,' '));
+
+    pm.add( {p2:'v3'}, 'r3' ); //sys.puts(pm.toString());
+    assert.equal('r3',pm.find({p2:'v3'}));
+    assert.equal(null,pm.find({p2:'v2'}));
+    assert.equal(null,pm.find({p2:'v1'}));
+    assert.equal('PropMap:root:\n * -> p1:\n v1 -> :\n r1x\n v2 -> :\n r2\n * -> p2:\n v3 -> :\n r3\n',pm.toString().replace(/ +/g,' '));
+
+    pm.add( {p1:'v1',p3:'v4'}, 'r4' ); //sys.puts(pm.toString());
+    assert.equal('r4',pm.find({p1:'v1',p3:'v4'}));
+    assert.equal('r1x',pm.find({p1:'v1',p3:'v5'}));
+    assert.equal(null,pm.find({p2:'v1'}));
+    assert.equal('PropMap:root:\n * -> p1:\n v1 -> p3:\n v4 -> :\n r4\n * -> :\n r1x\n v2 -> :\n r2\n * -> p2:\n v3 -> :\n r3\n',pm.toString().replace(/ +/g,' '));
     
   }
-
 }
