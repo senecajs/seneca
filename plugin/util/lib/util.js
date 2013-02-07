4409376e4bf503e2b1ccc70bbc203ff1cfa22f08
Richard Rodger <richard@ricebridge.com>
2013-02-07 18:26:32 +0000

    using nid to generate ids in util plugin

--- plugin/util/lib/util.js
+++ plugin/util/lib/util.js
@@ -1,11 +1,15 @@
 /* Copyright (c) 2011-2013 Richard Rodger */
+"use strict";
+
+var nid = require('nid')
 
 
 module.exports = function(seneca,opts,cb) {
-
   var name = 'util'
 
+  // legacy cmd
   seneca.add({role:name,cmd:'quickcode'},function(args,cb){
+    args.len = args.length || args.len
     var len      = args.len ? parseInt(args.len,10) : 8
     var alphabet = args.alphabet || '0123456789abcdefghijklmnopqrstuvwxyz'
     var curses   = args.curses || ['\x66\x75\x63\x6B',
@@ -14,37 +18,32 @@ module.exports = function(seneca,opts,cb) {
                                    '\x63\x75\x6E\x74',
                                    '\x6E\x69\x67\x67\x65\x72']
     
-    var numchars = alphabet.length
+    var nidopts = {}
+    if( len ) nidopts.length = len;
+    if( alphabet ) nidopts.alphabet = alphabet;
+    if( curses ) nidopts.curses = curses;
 
-    var code = null
-    while( cursed(code) ) {
-      var time = new Date().getTime()
-      var sb = []
-      for(var i = 0; i < len; i++) {
-        var c = Math.floor((time * Math.random()) % numchars)
-        sb.push( alphabet[c] ) 
-      }
-      code = sb.join('')
-    }
+    var actnid = nid(nidopts)
 
-
-    function cursed(code) {
-      if( code ) {
-        for( var i = 0; i < curses.length; i++ ) {
-          if( -1 != code.indexOf(curses[i]) ) {
-              return true
-          }
-        }
-        return false
-        }
-      else {
-        return true
-      }
-    }
-    
-    cb(null,code)
-      
+    cb(null,actnid())
   })
+
+
+  // cache nid funcs up to length 64
+  var nids = []
+  
+  seneca.add({role:name,cmd:'generate_id'},function(args,cb){
+    var actnid, length = args.length || 6
+    if( length < 65 ) {
+      actnid = nids[length] || (nids[length]=nid({length:length}))
+    }
+    else {
+      actnid = nid({length:length})
+    }
+
+    cb(null,actnid())
+  })
+
 
   cb(null,{
     name:name,
--- test/plugin/util.test.js
+++ test/plugin/util.test.js
@@ -24,4 +24,18 @@ describe('util', function() {
       assert.isNull( /[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/.exec(code) )
     })
   })
+
+  it('generate_id', function() {
+    util.generate_id({},function(err,code){
+      assert.isNull(err)
+      assert.equal( 6, code.length )
+      assert.isNotNull( /^[0-9a-z]{6,6}$/.exec(code) )
+    })
+    util.generate_id({length:4},function(err,code){
+      assert.isNull(err)
+      assert.equal( 4, code.length )
+      assert.isNotNull( /^[0-9a-z]{4,4}$/.exec(code) )
+    })
+  })
+
 })
