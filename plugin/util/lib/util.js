ab19d193c95e6985f8182749bae9785153414767
Richard Rodger <richard@ricebridge.com>
2013-06-19 19:13:01 +0100

    define_sys_entity, fix make$ to handle canon

--- plugin/util/lib/util.js
+++ plugin/util/lib/util.js
@@ -5,16 +5,47 @@
 var fs = require('fs')
 var util = require('util')
 
-var nid = require('nid')
-var _   = require('underscore')
+var nid   = require('nid')
+var _     = require('underscore')
+var async = require('async')
 
 
-module.exports = function(opts,register) {
+function nil(){
+  _.each(arguments,function(arg){
+    if( _.isFunction(arg) ) {
+      return arg()
+    }
+  })
+}
+
+
+
+module.exports = function( options ) {
   var name = 'util'
   var pluginseneca = this
 
+
+  options = this.util.deepextend({
+    limit: { parallel: 11 }
+  },options)
+
+
   // legacy cmd
-  this.add({role:name,cmd:'quickcode'},function(args,done){
+  this.add({role:name,cmd:'quickcode'},cmd_quickcode)
+  
+  this.add({role:name,cmd:'generate_id'},cmd_generate_id)
+
+  this.add(
+    {role:name,cmd:'ensure_entity'},
+    { required$:['pin','entmap'],
+      pin:{type$:'object'},
+      entmap:{type$:'object'} }, ensure_entity)
+
+  this.add({role:name,cmd:'define_sys_entity'},cmd_define_sys_entity)
+
+
+
+  function cmd_quickcode(args,done){
     args.len = args.length || args.len
     var len      = args.len ? parseInt(args.len,10) : 8
     var alphabet = args.alphabet || '0123456789abcdefghijklmnopqrstuvwxyz'
@@ -28,14 +59,14 @@ module.exports = function(opts,register) {
     var actnid = nid(nidopts)
 
     done(null,actnid())
-  })
+  }
 
 
   // cache nid funcs up to length 64
   var nids = []
   
   // TODO: allow specials based on ent canon: name,base,zone props
-  this.add({role:name,cmd:'generate_id'},function(args,done){
+  function cmd_generate_id(args,done){
     var actnid, length = args.length || 6
     if( length < 65 ) {
       actnid = nids[length] || (nids[length]=nid({length:length}))
@@ -45,59 +76,95 @@ module.exports = function(opts,register) {
     }
 
     done(null,actnid())
-  })
+  }
 
 
 
 
-  this.add(
-    {role:name,cmd:'ensure_entity'},
-    { required$:['pin','entmap'],
-      pin:{type$:'object'},
-      entmap:{type$:'object'} },
+  function ensure_entity(args,done){
+    var entmap = args.entmap
 
-    function(args,done){
-      var entmap = args.entmap
+    pluginseneca.wrap(args.pin,function(args,done){
+      var seneca = this
+      seneca.util.recurse(
+        _.keys(entmap),
+        function(entarg,next){
+          //console.log('work entarg:'+entarg+' args:'+util.inspect(args))
 
-      pluginseneca.wrap(args.pin,function(args,done){
-        var seneca = this
-        seneca.util.recurse(
-          _.keys(entmap),
-          function(entarg,next){
-            //console.log('work entarg:'+entarg+' args:'+util.inspect(args))
-
-            // ent id
-            if( _.isString(args[entarg]) ) {
-              entmap[entarg].load$( args[entarg], function(err,ent){
-                if(err) return done(err)
-                args[entarg]=ent
-                return next(null,args)
-              })
-            }
-            
-            // ent JSON - contains $:{name,base,zone}
-            else if( _.isObject(args[entarg]) && args[entarg].$ ) {
-              args[entarg] = entmap[entarg].make$(args[entarg]) 
+          // ent id
+          if( _.isString(args[entarg]) ) {
+            entmap[entarg].load$( args[entarg], function(err,ent){
+              if(err) return done(err)
+              args[entarg]=ent
               return next(null,args)
-            }
-            
-            else return next(null,args);
-          },
-          function(err,args) {
-            //console.log('done: '+util.inspect(args))
-            if( err ) return done(err);
-            return seneca.parent(args,done)
+            })
           }
-        )
-      })
-
-      done()
+          
+          // ent JSON - contains $:{name,base,zone}
+          else if( _.isObject(args[entarg]) && args[entarg].$ ) {
+            args[entarg] = entmap[entarg].make$(args[entarg]) 
+            return next(null,args)
+          }
+          
+          else return next(null,args);
+        },
+        function(err,args) {
+          //console.log('done: '+util.inspect(args))
+          if( err ) return done(err);
+          return seneca.parent(args,done)
+        }
+      )
     })
 
+    done()
+  }
 
+
+  
+  function cmd_define_sys_entity(args,done) {
+    var seneca = this
+    var list = args.list || [_.pick(args,['entity','zone','base','name','fields'])]
+
+    //console.dir(list)
+    
+    var sys_entity = seneca.make$('sys','entity')
+
+    function define(entry,next) {
+      if( _.isString(entry) ) {
+        entry = seneca.util.parsecanon(entry)
+      }
+      else if( _.isString(entry.entity) ) {
+        var fields = entry.fields
+        entry = seneca.util.parsecanon(entry.entity)
+        entry.fields = fields
+      }
+
+      //console.dir(entry)
+
+      var entq = {zone:entry.zone,base:entry.base,name:entry.name}
+      sys_entity.load$(entq,function(err,ent){
+        if(err) return next(err);
+
+        if( void 0 == entry.fields ) {
+          entry.fields = []
+        }
+
+        (ent?nil:sys_entity.save$)(entry,function(err,ent){
+          return next(err,ent)
+        })
+      })
+    }
+
+    async.mapLimit( args.list || [], options.limit.parallel, define, done )
+  }
+
+
+
+
+  // TODO: needs own plugin, and more generic than just util
   var browser_js
 
-  register(null,{
+  return {
     name:name,
     service: function(req,res,next) {
 
@@ -124,7 +191,7 @@ module.exports = function(opts,register) {
       }
       else next();
     }
-  })
+  }
 }
 
 
--- test/entity.test.js
+++ test/entity.test.js
@@ -14,6 +14,29 @@ var assert  = require('chai').assert
 
 describe('entity', function(){
 
+  it('parsecanon', function(){
+    var si = seneca()
+    function def(v,d){return void 0 == v ? d : v}
+    function fmt(cn){ return def(cn.zone,'-')+'/'+def(cn.base,'-')+'/'+def(cn.name,'-') }
+
+    assert.equal('-/-/n1',fmt(si.util.parsecanon('n1')))
+    assert.equal('-/b1/n1',fmt(si.util.parsecanon('b1/n1')))
+    assert.equal('z1/b1/n1',fmt(si.util.parsecanon('z1/b1/n1')))
+
+    assert.equal('-/-/-',fmt(si.util.parsecanon('-')))
+    assert.equal('-/-/-',fmt(si.util.parsecanon('-/-')))
+    assert.equal('-/-/-',fmt(si.util.parsecanon('-/-/-')))
+    assert.equal('-/-/0',fmt(si.util.parsecanon('0')))
+    assert.equal('-/0/0',fmt(si.util.parsecanon('0/0')))
+    assert.equal('0/0/0',fmt(si.util.parsecanon('0/0/0')))
+
+    var fail
+    try { si.util.parsecanon(''); fail = '' } catch(e) {}
+    try { si.util.parsecanon('?'); fail = '?' } catch(e) {}
+    assert.ok( void 0 == fail, fail )
+  })
+
+
   it('make', function(){
     var si = seneca()
 
@@ -27,6 +50,25 @@ describe('entity', function(){
     assert.equal("{ zone: undefined, base: undefined, name: 'foo' }",util.inspect(foo.canon$({object:true})))
     assert.equal("{ 'zone$': undefined, 'base$': undefined, 'name$': 'foo' }",util.inspect(foo.canon$({object$:true})))
     assert.equal(',,foo',''+foo.canon$({}))
+
+    var b1_n1 = si.make$('b1/n1')
+    assert.equal('-/b1/n1',b1_n1.entity$)
+    var z1_b1_n1 = si.make$('z1/b1/n1')
+    assert.equal('z1/b1/n1',z1_b1_n1.entity$)
+
+    var pe = si.make({entity$:'-/-/a'})
+    assert.equal('-/-/a',pe.entity$)
+    pe = si.make({entity$:'-/b/a'})
+    assert.equal('-/b/a',pe.entity$)
+    pe = si.make({entity$:'c/b/a'})
+    assert.equal('c/b/a',pe.entity$)
+
+    var ap = si.make$('a',{x:1})
+    assert.equal('-/-/a',ap.entity$)
+    ap = si.make$('b','a',{x:1})
+    assert.equal('-/b/a',ap.entity$)
+    ap = si.make$('c','b','a',{x:1})
+    assert.equal('c/b/a',ap.entity$)
   })
 
   
@@ -41,7 +83,8 @@ describe('entity', function(){
     si.make$('c','b','a',{x:3}).save$(function(e,o){x3=o})
 
     si.act('role:mem-store,cmd:dump',function(e,o){
-      assert.ok( gex('{"undefined":{"a":{"*":{"entity$":"-/-/a","x":1,"id":"*"}}},"b":{"a":{"*":{"entity$":"-/b/a","x":2,"id":"*"},"*":{"entity$":"c/b/a","x":3,"id":"*"}}}}').on(JSON.stringify(o)) )
+      var t = gex('{"undefined":{"a":{"*":{"entity$":"-/-/a","x":1,"id":"*"}}},"b":{"a":{"*":{"entity$":"-/b/a","x":2,"id":"*"},"*":{"entity$":"c/b/a","x":3,"id":"*"}}}}').on(JSON.stringify(o))
+      assert.ok(t)
     })
 
     si.act('role:mem-store,cmd:export',{file:'mem.json'}, function(e){
