2efe75edd0140069b285887693b5e675a1e8a5e2
Richard Rodger <richard@ricebridge.com>
2014-02-23 21:09:12 +0000

    intro norma

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -17,25 +17,56 @@ describe('plugin', function(){
   it('depends', function(){
     var si = seneca()
     
-    si.use( function(opts,reg){
-      reg(null,{name:'aaa'})
+    si.use( function(opts){
+      return {name:'aaa'}
     })
 
-    si.use( function(opts,reg){
+    si.use( function(opts){
       this.depends('bbb',['aaa'])
-      reg(null,{name:'bbb'})
+      return {name:'bbb'}
     })
 
     try {
-      si.use( function(opts,reg){
+      si.use( function(opts){
         this.depends('ccc',['zzz'])
-        reg(null,{name:'ccc'})
+        return {name:'ccc'}
       })
       assert.fail()
     }
     catch( e ) {
       assert.equal('seneca/plugin_required',e.seneca.code)
     }
+
+    si.use( function(opts){
+      return {name:'ddd'}
+    })
+
+    si.use( function(opts){
+      this.depends('eee','aaa')
+      return {name:'eee'}
+    })
+
+    si.use( function(opts){
+      this.depends('fff',['aaa','ddd'])
+      return {name:'fff'}
+    })
+
+    si.use( function(opts){
+      this.depends('ggg','aaa','ddd')
+      return {name:'ggg'}
+    })
+
+    try {
+      si.use( function(opts){
+        this.depends('hhh','aaa','zzz')
+        return {name:'hhh'}
+      })
+      assert.fail()
+    }
+    catch( e ) {
+      assert.equal('seneca/plugin_required',e.seneca.code)
+    }
+
   })
 
 })
