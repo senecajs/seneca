e998c240abe341196418fb3b8f0a8169dca50718
Richard Rodger <richard@ricebridge.com>
2014-07-13 07:59:35 +0100

    test included plugins

---
+++ test/test-included-plugins.sh
@@ -0,0 +1,21 @@
+echo ---seneca-basic---
+cd ../../seneca-basic
+npm test
+
+echo ---seneca-echo---
+cd ../seneca-echo
+npm test
+
+echo ---seneca-mem-store---
+cd ../seneca-mem-store
+npm test
+
+echo ---seneca-transport---
+cd ../seneca-transport
+npm test
+
+echo ---seneca-web---
+cd ../seneca-web
+npm test
+
+
