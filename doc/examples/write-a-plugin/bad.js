1e1b3c390084a0cff1dfefdeeb76cc17865d011e
Richard Rodger <richard@ricebridge.com>
2014-08-25 16:14:08 +0100

    executor log msgs

--- doc/examples/write-a-plugin/bad.js
+++ doc/examples/write-a-plugin/bad.js
@@ -6,6 +6,10 @@ function bad( options ) {
   this.log.error( 'something is almost, but not quite, correct' )
 
   
+  this.add('role:bad, err:exception', function(args,done) {
+    throw new Error('will it be my friend?')
+  })
+
   this.add('role:bad, err:action', function(args,done) {
     done( new Error('not again...') )
   })
@@ -21,5 +25,6 @@ var seneca = require('../../../lib/seneca.js')()
 
 seneca.use( bad, {color:'intelligent blue'} )
 
-seneca.act( 'role:bad, err:action, question:meaning' )
-seneca.act( 'role:bad, cmd:normal, answer:42' )
+seneca.act( 'role:bad, err:exception, improbability:infinite' )
+//seneca.act( 'role:bad, err:action, question:meaning' )
+//seneca.act( 'role:bad, cmd:normal, answer:42' )
