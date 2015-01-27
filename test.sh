bd1ff34c9b729a4be993f0d555e749291c569ab8
Richard Rodger <richard@ricebridge.com>
2015-01-27 12:48:27 +0000

    test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+if [ ! -d "./node_modules/mocha" ]; then
+  npm install mocha@1
+fi
+if [ ! -d "./node_modules/connect" ]; then
+  npm install connect@3
+fi
+if [ ! -d "./node_modules/connect-query" ]; then
+  npm install connect-query@0
+fi
+if [ ! -d "./node_modules/docco" ]; then
+  npm install docco@0
+fi
+if [ ! -d "./node_modules/jshint" ]; then
+  npm install jshint@2
+fi
+if [ ! -d "./node_modules/fixture-stdout" ]; then
+  npm install fixture-stdout@0
+fi
+if [ ! -d "./node_modules/seneca-error-test" ]; then
+  npm install seneca-error-test@0
+fi
+
+./node_modules/.bin/mocha test/*.test.js test/plugin/*.test.js test/options/*.test.js
