b76ca56a8f4a3219c69d3ce9ec9b63e201ace1d7
Richard Rodger <richard@ricebridge.com>
2015-01-28 17:20:29 +0000

    seneca-transport 0.6.1

--- test.sh
+++ test.sh
@@ -7,12 +7,6 @@ fi
 if [ ! -d "./node_modules/connect-query" ]; then
   npm install connect-query@0
 fi
-if [ ! -d "./node_modules/docco" ]; then
-  npm install docco@0
-fi
-if [ ! -d "./node_modules/jshint" ]; then
-  npm install jshint@2
-fi
 if [ ! -d "./node_modules/fixture-stdout" ]; then
   npm install fixture-stdout@0
 fi
