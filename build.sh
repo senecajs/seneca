cfcb312f26f7c0abab8353fc6a5fe34a7112c444
Richard Rodger <richard@ricebridge.com>
2015-01-29 11:27:47 +0000

    norma 0.3.0; use-plugin 0.3.1; devdeps fixed

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-if [ ! -d "./node_modules/docco" ]; then
-  npm install docco@0
-fi
-if [ ! -d "./node_modules/jshint" ]; then
-  npm install jshint@2
-fi
 ./node_modules/.bin/jshint seneca.js lib/*.js
 ./node_modules/.bin/docco seneca.js -o doc
 cp -r doc/* ../gh-pages/seneca/doc
