5b28a1c3b3035615f40baab712848b4d439e07fd
Wyatt Preul <wpreul@gmail.com>
2015-09-30 10:30:08 -0500

    Seneca can now be decorated/extended with new properties in a safe way

--- test/seneca.test.js
+++ test/seneca.test.js
@@ -1335,7 +1335,6 @@ describe('seneca', function () {
     })
   })
 
-
   it('add-noop',function(done){
     seneca({log:'silent'})
       .error(done)
@@ -1351,4 +1350,51 @@ describe('seneca', function () {
       })
   })
 
+  describe('#decorate', function () {
+    it('can add a property to seneca', function (done) {
+      var si = seneca({ log: 'silent' })
+      si.decorate('foo', function () {
+        return 'bar'
+      })
+
+      assert.equal(si.foo(), 'bar')
+      done()
+    })
+
+    it('cannot override core property', function (done) {
+      var si = seneca({ log: 'silent' })
+
+      var fn = function () {
+        si.decorate('use', 'foo')
+      }
+
+      assert.throws(fn)
+      done()
+    })
+
+    it('cannot overwrite a decorated property', function (done) {
+      var si = seneca({ log: 'silent' })
+      si.decorate('foo', function () {
+        return 'bar'
+      })
+
+      var fn = function () {
+        si.decorate('foo', 'bar')
+      }
+
+      assert.throws(fn)
+      done()
+    })
+
+    it('cannot prefix a property with an underscore', function (done) {
+      var si = seneca({ log: 'silent' })
+
+      var fn = function () {
+        si.decorate('_use', 'foo')
+      }
+
+      assert.throws(fn)
+      done()
+    })
+  })
 })
