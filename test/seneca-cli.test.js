a9ed89ba54e546e555d8c34d10223d7f7b62ffb2
Wyatt Preul <wpreul@gmail.com>
2015-11-02 20:25:10 -0600

    Can't run exec in 0.10 with env override

--- test/seneca-cli.test.js
+++ test/seneca-cli.test.js
@@ -14,14 +14,6 @@ var it = lab.it
 describe('CLI', function () {
   var launchPath = path.join(__dirname, 'launch')
 
-  it('respects seneca.print.tree[.all] arg', function (done) {
-    childProcess.exec(process.argv[0] + ' ' + launchPath + ' --seneca.print.tree', { env: { 'SENECA_LOG': 'all' } }, function (err, stdout, stderr) {
-      assert(!err)
-      assert(stdout.indexOf('Seneca') !== -1)
-      done()
-    })
-  })
-
   it('won\'t display action patterns message when they aren\'t provided', function (done) {
     childProcess.exec(process.argv[0] + ' ' + launchPath, function (err, stdout, stderr) {
       assert(!err)
