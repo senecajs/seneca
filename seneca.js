601beb51efcfb3fcc419a33b0343b61a107c0f60
Wyatt Preul <wpreul@gmail.com>
2015-11-20 15:29:09 -0600

    Pull plugin register into plugin util

--- seneca.js
+++ seneca.js
@@ -31,7 +31,7 @@ var Logging = require('./lib/logging')
 var MakeEntity = require('./lib/entity')
 var Optioner = require('./lib/optioner')
 var Package = require('./package.json')
-var PluginUtil = require('./lib/plugin-util')
+var Plugins = require('./lib/plugins')
 var Print = require('./lib/print')
 var Repl = require('./lib/repl')
 var Store = require('./lib/store')
@@ -274,7 +274,7 @@ function make_seneca (initial_options) {
 
   // Non-API methods.
   root.logroute = api_logroute
-  root.register = api_register
+  root.register = Plugins.register(so, private$, paramcheck, makedie, callpoint)
   root.depends = api_depends
   root.cluster = api_cluster
   root.hasplugin = api_hasplugin
@@ -406,7 +406,6 @@ function make_seneca (initial_options) {
     nil: Common.nil,
     argprops: Common.argprops,
     print: Common.print,
-
     router: function () { return Patrun() },
     parsecanon: MakeEntity.parsecanon
   }
@@ -438,125 +437,6 @@ function make_seneca (initial_options) {
 
     entry.handler = handler || entry.handler
     Logging.makelogroute(entry, root.log.router)
-  }
-
-  function api_register (plugin) {
-    var self = this
-
-    paramcheck.register.validate(plugin, thrower)
-
-    var fullname = plugin.name + (plugin.tag ? '/' + plugin.tag : '')
-
-    plugin.fullname = fullname
-
-    var sd = PluginUtil.make_delegate(
-      self,
-      plugin,
-      {makedie: makedie}
-   )
-
-    self.log.debug('register', 'init', fullname, callpoint())
-
-    var plugin_options = PluginUtil.resolve_options(fullname, plugin, so)
-
-    sd.log.debug('DEFINE', plugin_options)
-
-    var meta
-    try {
-      meta = PluginUtil.define_plugin(sd, plugin, plugin_options)
-    }
-    catch (e) {
-      return sd.die(e)
-    }
-
-    // legacy api for service function
-    if (_.isFunction(meta)) {
-      meta = {service: meta}
-    }
-
-    plugin.name = meta.name || plugin.name
-    plugin.tag =
-      meta.tag ||
-      plugin.tag ||
-      (plugin.options && plugin.options.tag$)
-
-    plugin.fullname = plugin.name + (plugin.tag ? '/' + plugin.tag : '')
-
-    plugin.service = meta.service || plugin.service
-
-    sd.__update_plugin__(plugin)
-
-    var pluginref = plugin.name + (plugin.tag ? '/' + plugin.tag : '')
-    private$.plugins[pluginref] = plugin
-
-    private$.plugin_order.byname.push(plugin.name)
-    private$.plugin_order.byname = _.uniq(private$.plugin_order.byname)
-
-    private$.plugin_order.byref.push(pluginref)
-
-    // LEGACY
-    var service = plugin.service
-    if (service) {
-      service.log = sd.log
-      service.key = pluginref
-      self.act('role:web', {use: service})
-    }
-
-    self.act(
-      {
-        init: plugin.name,
-        tag: plugin.tag,
-        default$: {},
-        gate$: true,
-        fatal$: true,
-        local$: true
-      },
-      function (err, out) {
-        if (err) {
-          var plugin_err_code = 'plugin_init'
-
-          plugin.plugin_error = err.message
-
-          if (err.code === 'action-timeout') {
-            plugin_err_code = 'plugin_init_timeout'
-            plugin.timeout = so.timeout
-          }
-
-          return self.die(internals.error(err, plugin_err_code, plugin))
-        }
-
-        if (so.debug.print && so.debug.print.options) {
-          console_log('\nSeneca Options (' + self.id + '): plugin: ' + plugin.name +
-            (plugin.tag ? '$' + plugin.tag : '') + '\n' +
-            '===\n')
-          console_log(Util.inspect(plugin_options, {depth: null}))
-          console_log('')
-        }
-
-        return self.log.debug('register', 'ready', pluginref, out)
-      }
-   )
-
-    var exports = []
-
-    if (meta.export !== void 0) {
-      private$.exports[pluginref] = meta.export
-      exports.push(pluginref)
-    }
-
-    if (_.isObject(meta.exportmap) || _.isObject(meta.exports)) {
-      meta.exportmap = meta.exportmap || meta.exports
-      _.each(meta.exportmap, function (v, k) {
-        if (v !== void 0) {
-          var exportname = pluginref + '/' + k
-          private$.exports[exportname] = v
-          exports.push(exportname)
-        }
-      })
-    }
-
-    self.log.debug('register', 'install', pluginref,
-      { exports: exports }, fullname !== pluginref ? fullname : undefined)
   }
 
   function api_depends () {
