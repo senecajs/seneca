26d0a9c2f68bc9b1186d2fac3a58bda6b749168e
Wyatt Preul <wpreul@gmail.com>
2016-01-16 12:13:27 -0600

    Fix more tests

--- test/plugin/basic.test.js
+++ test/plugin/basic.test.js
@@ -9,64 +9,69 @@ var lab = exports.lab = Lab.script()
 var describe = lab.describe
 var it = lab.it
 var assert = Assert
-var si = Seneca({log: 'silent'})
-var util = si.pin({role: 'util', cmd: '*'})
 
 
 describe('util', function () {
   it('generate_id', function (fin) {
-    si.options({errhandler: fin})
-    util.generate_id({}, function (err, code) {
-      assert.equal(err, null)
-      assert.equal(6, code.length)
-      assert.ok(/^[0-9a-z]{6,6}$/.exec(code))
-
-      util.generate_id({length: 4}, function (err, code) {
+    var si = Seneca({log: 'silent'})
+    si.once('ready', function () {
+      var util = si.pin({role: 'util', cmd: '*'})
+      si.options({errhandler: fin})
+      util.generate_id({}, function (err, code) {
         assert.equal(err, null)
-        assert.equal(4, code.length)
-        assert.ok(/^[0-9a-z]{4,4}$/.exec(code))
+        assert.equal(6, code.length)
+        assert.ok(/^[0-9a-z]{6,6}$/.exec(code))
 
-        fin()
+        util.generate_id({length: 4}, function (err, code) {
+          assert.equal(err, null)
+          assert.equal(4, code.length)
+          assert.ok(/^[0-9a-z]{4,4}$/.exec(code))
+
+          fin()
+        })
       })
     })
   })
 
   it('ensure_entity', function (fin) {
-    si.options({errhandler: fin})
+    var si = Seneca({log: 'silent'})
+    si.once('ready', function () {
+      var util = si.pin({role: 'util', cmd: '*'})
+      si.options({errhandler: fin})
+      var foo_ent = si.make$('util_foo')
+      var fooid = {}
+      var foos = []
+      foo_ent.make$({a: 1}).save$(function (e, o) {
+        fooid[1] = o.id; foos.push(o)
 
-    var foo_ent = si.make$('util_foo')
-    var fooid = {}
-    var foos = []
-    foo_ent.make$({a: 1}).save$(function (e, o) {
-      fooid[1] = o.id; foos.push(o)
+        foo_ent.make$({a: 2}).save$(function (e, o) {
+          fooid[2] = o.id; foos.push(o)
 
-      foo_ent.make$({a: 2}).save$(function (e, o) {
-        fooid[2] = o.id; foos.push(o)
+          si.add({bar: 1, cmd: 'A'}, function (args, done) {
+            var foo = args.foo
+            foo.a = 10 * foo.a
+            foo.save$(done)
+          })
 
-        si.add({bar: 1, cmd: 'A'}, function (args, done) {
-          var foo = args.foo
-          foo.a = 10 * foo.a
-          foo.save$(done)
-        })
+          si.act({
+            role: 'util', cmd: 'ensure_entity',
+            pin: {bar: 1, cmd: '*'},
+            entmap: { foo: foo_ent }
+          }, function () {
+            // just use ent if given
+            si.act({bar: 1, cmd: 'A', foo: foos[0]}, function (e, o) {
+              assert.equal(10, o.a)
 
-        si.act({
-          role: 'util', cmd: 'ensure_entity',
-          pin: {bar: 1, cmd: '*'},
-          entmap: { foo: foo_ent }
-        }, function () {
-          // just use ent if given
-          si.act({bar: 1, cmd: 'A', foo: foos[0]}, function (e, o) {
-            assert.equal(10, o.a)
+              // load from id
+              si.act({bar: 1, cmd: 'A', foo: fooid[1]}, function (e, o) {
+                assert.equal(100, o.a)
 
-            // load from id
-            si.act({bar: 1, cmd: 'A', foo: fooid[1]}, function (e, o) {
-              assert.equal(100, o.a)
+                // initialize from data
+                si.act({bar: 1, cmd: 'A', foo: foos[1].data$()}, function (e, o) {
+                  assert.equal(20, o.a)
 
-              // initialize from data
-              si.act({bar: 1, cmd: 'A', foo: foos[1].data$()}, function (e, o) {
-                assert.equal(20, o.a)
-
-                fin()
+                  fin()
+                })
               })
             })
           })
@@ -76,6 +81,7 @@ describe('util', function () {
   })
 
   it('note', function (fin) {
+    var si = Seneca({log: 'silent'})
     si
       .start(fin)
       .wait('role:util,note:true,cmd:set,key:foo,value:red')
