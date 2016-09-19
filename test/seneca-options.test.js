b43ba93bceeec05f6ac33e43509532dff0d72515
Mihai Dima <mihaizn@gmail.com>
2016-09-19 11:45:18 +0300

    support for aliases like seneca.log.all

--- test/seneca-options.test.js
+++ test/seneca-options.test.js
@@ -82,60 +82,102 @@ describe('seneca --seneca.log arguments tests', function () {
 
     done()
   })
-
-  it('incorrect arg --seneca.log.all:', function (done) {
-    process.argv = ['', '', '--seneca.log.all']
-    var si = Seneca()
-    expect(_.isMatch(si.export('options').log, {})).to.be.true()
-
-    done()
-  })
 })
 
 describe('seneca --seneca.log aliases tests', function () {
   it('--seneca.log.quiet', function (done) {
-    process.argv = ['', '', '--seneca.log.level.quiet']
+    process.argv = ['', '', '--seneca.log.quiet']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'quiet' })).to.be.true()
 
     done()
   })
   it('--seneca.log.silent', function (done) {
-    process.argv = ['', '', '--seneca.log.level.silent']
+    process.argv = ['', '', '--seneca.log.silent']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'silent' })).to.be.true()
 
     done()
   })
   it('--seneca.log.all', function (done) {
-    process.argv = ['', '', '--seneca.log.level.all']
+    process.argv = ['', '', '--seneca.log.all']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'all' })).to.be.true()
 
     done()
   })
   it('--seneca.log.any', function (done) {
-    process.argv = ['', '', '--seneca.log.level.any']
+    process.argv = ['', '', '--seneca.log.any']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'any' })).to.be.true()
 
     done()
   })
   it('--seneca.log.print', function (done) {
-    process.argv = ['', '', '--seneca.log.level.print']
+    process.argv = ['', '', '--seneca.log.print']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'print' })).to.be.true()
 
     done()
   })
   it('--seneca.log.test', function (done) {
-    process.argv = ['', '', '--seneca.log.level.test']
+    process.argv = ['', '', '--seneca.log.test']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'test' })).to.be.true()
 
     done()
   })
   it('--seneca.log.standard', function (done) {
+    process.argv = ['', '', '--seneca.log.standard']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'standard' })).to.be.true()
+
+    done()
+  })
+
+  it('--seneca.log.level.quiet', function (done) {
+    process.argv = ['', '', '--seneca.log.level.quiet']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'quiet' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.silent', function (done) {
+    process.argv = ['', '', '--seneca.log.level.silent']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'silent' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.all', function (done) {
+    process.argv = ['', '', '--seneca.log.level.all']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'all' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.any', function (done) {
+    process.argv = ['', '', '--seneca.log.level.any']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'any' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.print', function (done) {
+    process.argv = ['', '', '--seneca.log.level.print']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'print' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.test', function (done) {
+    process.argv = ['', '', '--seneca.log.level.test']
+    var si = Seneca()
+    expect(_.isMatch(si.export('options').log, { level: 'test' })).to.be.true()
+
+    done()
+  })
+  it('--seneca.log.level.standard', function (done) {
     process.argv = ['', '', '--seneca.log.level.standard']
     var si = Seneca()
     expect(_.isMatch(si.export('options').log, { level: 'standard' })).to.be.true()
