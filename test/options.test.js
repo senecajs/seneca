e5f06771d281f97d574034f5ee0821732a455e2f
Richard Rodger <richard@ricebridge.com>
2018-05-12 18:27:21 +0100

    use-plugin@1.0 options.test

---
+++ test/options.test.js
@@ -0,0 +1,21 @@
+/* Copyright (c) 2018 Richard Rodger and other contributors, MIT License */
+'use strict'
+
+var Code = require('code')
+var Lab = require('lab')
+
+var lab = (exports.lab = Lab.script())
+var describe = lab.describe
+var it = lab.it
+var expect = Code.expect
+
+var Seneca = require('..')
+
+describe('options', function() {
+  it('strict.find', function(fin) {
+    Seneca({strict: {find: false}})
+      .test(fin)
+      .act('foo:1')
+      .ready(fin)
+  })
+})
