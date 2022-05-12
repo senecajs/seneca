3f637a2ef65e924332ab05b8f18d664087e627d5
Richard Rodger <richard@ricebridge.com>
2022-05-12 17:54:43 +0100

    update nid

---
+++ test/fatal.js
@@ -0,0 +1,25 @@
+
+
+const Seneca = require('..')
+
+
+function pe1() {
+  throw new Error('AA')
+}
+
+
+const seneca = Seneca({
+  legacy:false,
+  log:{level:'debug',logger:'flat'},
+  death_delay: 1,
+})
+      // .use(pe1)
+
+      .add('a:1', function() {
+        throw new Error('A1')
+      })
+      .listen()
+      .ready(function() {
+        this.act('a:1', {fatal$:true}, Seneca.util.print)
+      })
+
